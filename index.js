65e59ab3beffd030bfc02b2edcb8a6f7e1e6cf21
Dmitri Nesterenko <dmitri@sentimentalistmag.com>
2019-04-14 17:33:27 -0400

    Adds filtering
    Populates the data that is interesting from the spreadsheet

--- index.js
+++ index.js
@@ -2,7 +2,57 @@ const xlsx = require('xlsx')
 function fetch_data(nabe){
 	//stream = fs.createReadStream('data/' + nabe + '.xls')
 	var workbook = xlsx.readFile('data/rollingsales_' + nabe + '.xls')
-	return workbook
+	//For work book details query the Sheets object
+	//workbook.Sheets[workbook.SheetNames[0]] 
+	//the workbooks data starts with
+	//Borough -- A6-A100,000
+	//Neighborhood - B6-
+	//Building Class Category - C6 -
+	//Class at present - D6
+	//Block - E6
+	//Lot - F6
+	//Easement - G6
+	//Class at present - H6
+	//Address - I6
+	//Apartment number - J6
+	//Zip code K6
+	//Residential units - L6
+	//Commerical Units - M6
+	//Total units - N6
+	//Land square feet - O6
+	//Gross square feet - P6
+	//Year built - Q6
+	//Tax class - R6
+	//Building class - S6
+	//Sale price - T6
+	//Sale date - U6
+	//what we need
+	//Neighborhood, Address, zip code, total units, gross square feet, sale
+	//price and sale date
+	//B, I, K, N, P, T, U
+	//start with the magic number where these spreadsheets start their data
+	var i = 6
+	var data = []
+	var sheet = workbook.Sheets[workbook.SheetNames[0]]
+	do{
+	    	var total_units = 0
+	    	if(sheet["N" + i] !== undefined){
+			var total_units = sheet["N"+i].v
+		}
+		data[i-6] = {
+			Neighborhood: sheet["B"+i].v,
+			Price: sheet["T"+i].v,
+			Address: sheet["I"+i].v,
+			ZipCode: sheet["K"+i].v,
+			TotalUnits: sheet["N"+i].v,
+			SqFeet: sheet["P"+i].v,
+			SaleDate: sheet["U"+i].v
+		}
+		next_entry = workbook.Sheets[workbook.SheetNames[0]]["B"+i+1]
+		i = i + 1
+
+	}while(next_entry != undefined)
+	return data
 }
 function log(data){
     	//TODO add a verbose check before printing out 
@@ -12,29 +62,32 @@ function log(data){
 // squirrel({nabe,address,type}).median()
 // squirrel constructor acts as a filter from a params object
 squirrel = {
-    	data: {},
-	nabe: "",
-
-	filter: function(nabe){
-		this.nabe=nabe
-		this.data = fetch_data(nabe)
-		if(this.nabe == 'test'){
+    	data: [],
+	Borough: "",
+        filtered: [],	
+	filter: function(params){
+		this.Borough=params.Borough
+		//TODO add a "test" data set to the data folder
+		if(this.Borough == 'test'){
 			this.data = [100.00]
 		}else{
-			this.data = fetch_data(nabe)
+			this.data = fetch_data(this.Borough)
 		}
-		debugger
-		log(this.data.length)
+		this.filtered = this.data
+		if(params.ZipCode != undefined){
+			this.filtered = this.data.filter(sale => sale.ZipCode === params.ZipCode);
+		}
 		return this
 	},
 	median: function(){
-		var sum = 0.0
-		var length = this.data.length
+		var length = this.filtered.length
+		var sum = this.filtered.reduce((acc, sale) => acc + sale.Price, 0)
 		for(var i = 0; i<length; i++){
-			sum = sum + this.data[i]
+			sum = sum + this.data[i].Price
 		}
 		return sum / length
 	}	
 	
 }
 module.exports = squirrel
+squirrel.filter({Borough: 'brooklyn', ZipCode: '11222'})
