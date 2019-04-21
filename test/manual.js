c83d1c81725e0f6b4f63d86c0536b41b832a8aa7
Dmitri Nesterenko <dmitri@sentimentalistmag.com>
2019-04-21 15:23:57 -0400

    Fixes ESLint issues

--- test/manual.js
+++ test/manual.js
@@ -1,8 +1,7 @@
-//Manual testing scratch pad
-//primarily helps with debugging
+// Manual testing scratch pad
+// primarily helps with debugging
 const squirrel = require('../index')
-params = {Borough: 'test'}
-console.log(squirrel.filter(params).median() == 750000)
-//results = squirrel.filter({Borough: 'brooklyn', ZipCode: '11222'})
-//results.filtered.map(r => console.log(r))
-
+var params = { Borough: 'test' }
+console.log(squirrel.filter(params).median() === 750000)
+// results = squirrel.filter({Borough: 'brooklyn', ZipCode: '11222'})
+// results.filtered.map(r => console.log(r))
