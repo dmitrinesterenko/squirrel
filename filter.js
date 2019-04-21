95bb3f6a1800da2696e4bca68173f4103cc75586
Dmitri Nesterenko <dmitri@sentimentalistmag.com>
2019-04-21 16:54:11 -0400

    [WIP] read stream

--- filter.js
+++ filter.js
@@ -1,9 +1,9 @@
-//Read a stream, perform a filter operation on it and output the stream
-//- Duplex streams that can modify or transform the data as it is written and
-//read 
-const filter = {
-	const filter = (stream) => {
-		
-	}
-}
-module.exports = filter
+// Read a stream, perform a filter operation on it and output the stream
+// - Duplex streams that can modify or transform the data as it is written and
+// read
+// We want object mode streams so that we are transferring completed JS objects
+// back and forth
+const readStream = getMyInput()
+readStream.on('data', (chunk) => {
+  console.log(`Received ${chunk.length} bytes of data`)
+})
