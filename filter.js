b97d5e9726c99285d006ea4a389a1da3affcbcf8
Dmitri Nesterenko <dmitri@sentimentalistmag.com>
2019-04-21 16:44:04 -0400

    [WIP] Start breaking it up

---
+++ filter.js
@@ -0,0 +1,9 @@
+//Read a stream, perform a filter operation on it and output the stream
+//- Duplex streams that can modify or transform the data as it is written and
+//read 
+const filter = {
+	const filter = (stream) => {
+		
+	}
+}
+module.exports = filter
