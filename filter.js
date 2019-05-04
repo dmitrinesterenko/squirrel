cd0a5c6e2272aab1e220f3e297175840cc6b71a6
Dmitri Nesterenko <dmitri.nesterenko@gmail.com>
2019-05-04 10:38:15 -0400

    [WIP] Streams

--- filter.js
+++ filter.js
@@ -1,9 +1,17 @@
+const { Readable } = require('stream')
 // Read a stream, perform a filter operation on it and output the stream
 // - Duplex streams that can modify or transform the data as it is written and
 // read
 // We want object mode streams so that we are transferring completed JS objects
 // back and forth
-const readStream = getMyInput()
+const readStream = new Readable({
+  readableObjectMode: true})
+
 readStream.on('data', (chunk) => {
+  if(chunk.toString().indexOf('A') > 0){
+    console.log('I hear your message loud and clear')
+  }
   console.log(`Received ${chunk.length} bytes of data`)
 })
+
+process.stdin.pipe(readStream)
